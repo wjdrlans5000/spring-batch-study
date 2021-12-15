24e3888880955f2358a0d4bd448d17c4bd84fde3
= <wjdrlans4000@naver.com>
2021-12-15 23:32:12 +0900

    [Chapter01] 스프링배치 개요 및 Tasklet

---
+++ Chapter01/src/test/java/me/gimun/chapter01/Chapter01ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gimun.chapter01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
